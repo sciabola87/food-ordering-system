cac89e31f98d80dddd348f89f4722fc697ecbdd1
Sciabola87 <mouhsine.lahridi@gmail.com>
2022-07-16 16:36:56 +0200

    creazione DTO domain

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/input/service/OrderApplicationService.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/input/service/OrderApplicationService.java
@@ -3,6 +3,7 @@ package com.food.ordering.system.order.service.domain.ports.input.service;
 import com.food.ordering.system.order.service.domain.dto.create.CreateOrderCommand;
 import com.food.ordering.system.order.service.domain.dto.create.CreateOrderResponse;
 import com.food.ordering.system.order.service.domain.dto.track.TrackOrderQuery;
+import com.food.ordering.system.order.service.domain.dto.track.TrackOrderResponse;
 
 import javax.validation.Valid;
 
@@ -10,6 +11,6 @@ public interface OrderApplicationService {
 
     CreateOrderResponse createOrder(@Valid CreateOrderCommand createOrderCommand);
 
-    TrackOrderQuery trackOrder(@Valid TrackOrderQuery trackOrderQuery);
+    TrackOrderResponse trackOrder(@Valid TrackOrderQuery trackOrderQuery);
 
 }
